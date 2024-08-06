01414f5efbf76d744724cf69c2257315f94eadbc
Ceydaay <cydylmz.99@gmail.com>
2024-08-06 23:56:08 +0300

    Add project files.

---
+++ FirstClass/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClass
+{
+   public class Person
+    {
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public  DateTime BirthDate { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"ad:{FirstName},Soyad:{LastName}, Doğum Tarihi:{BirthDate.ToShortDateString()}");
+        }
+
+
+    }
+
+
+}
---
+++ FirstClass/Program.cs
@@ -0,0 +1,54 @@
+﻿using FirstClass;
+
+public class Program ()
+{
+    static void Main()
+    {
+        Person student1 = new Person()
+        {
+            FirstName = "Ceyda",
+            LastName = "Yılmaz",
+            BirthDate = new DateTime(1999, 2,7)
+        };
+
+        Person student2 = new Person()
+        {
+
+            FirstName = "Canberk",
+            LastName = "Doğan",
+            BirthDate = new DateTime(1999, 06, 21)
+        };
+
+        Person teacher1 = new Person()
+        {
+
+            FirstName = "Ayşe",
+            LastName = "Yılmaz",
+            BirthDate = new DateTime(1999, 7, 7)
+        };
+
+        Person teacher2 = new Person()
+        {
+
+            FirstName = "Tahir",
+            LastName = "Yılmaz",
+            BirthDate = new DateTime(1999, 10, 7)
+        };
+
+
+        Console.WriteLine("Students:");
+        student1.DisplayInfo();
+        student1.DisplayInfo();
+  
+
+
+        Console.WriteLine("Teachers:");
+        teacher1.DisplayInfo();
+        teacher2.DisplayInfo();
+
+
+
+    }
+}
+
+
